4efd4cd5da26ebbb2d0d45bcd6fe5dff36270d90
code-with-santanu <santanupal003@gmail.com>
2023-08-15 20:14:23 +0530

    initial commit

---
+++ Command_Line_Argument/Argument.java
@@ -0,0 +1,12 @@
+public class Argument {
+    public static void main(String[] args) {
+        for (String t : args) {
+            System.out.println(t);
+        }
+    }
+}
+
+
+// To see the output :
+
+//     java Argument <value1> <value 2> <value 3> ...<value n>
